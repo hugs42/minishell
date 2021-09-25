6ee1e980d8419cbe16400d086403092ba024a574
hugsbord <hugsbord@student.42.fr>
2021-09-25 15:44:59 +0200

    double quotes seems ok

--- srcs/get_absolute_path.c
+++ srcs/get_absolute_path.c
@@ -6,11 +6,95 @@
 /*   By: hugsbord <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 09:53:47 by hugsbord          #+#    #+#             */
-/*   Updated: 2021/09/24 20:15:14 by hugsbord         ###   ########.fr       */
+/*   Updated: 2021/09/25 15:43:57 by hugsbord         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/minishell.h"
+
+char	**ft_update_tab(t_data *data, char **new_cmd, int l, int count)
+{
+	int i;
+	int l_tmp;
+	int nb;
+	char **tmp_cmd;
+
+	i = 0;
+	nb = 0;
+	tmp_cmd = NULL;
+	l_tmp = l;
+	l++;
+	ft_putnbr_fd(44444, 1);
+	tmp_cmd = (char **)ft_calloc(count + 1, sizeof(char *));
+	while (new_cmd[i])
+	{
+		tmp_cmd[i] = ft_strdup(new_cmd[i]);
+		i++;
+	}
+//	i = 0;
+	while (data->cmd[l])
+	{
+		tmp_cmd[i] = ft_strdup(data->cmd[l]);
+//		free(data->cmd[l]);
+//		data->cmd[l] = NULL;
+//		data->cmd[l] = ft_strdup(new_cmd[i]);
+		l++;
+		i++;
+	}
+	int z = 0;
+	while (tmp_cmd[z])
+	{
+		ft_putstr_fd("@@@", 1);
+		ft_putstr_fd(tmp_cmd[z], 1);
+		ft_putstr_fd("\n", 1);
+		z++;
+	}
+	z = 0;
+	while (data->cmd[l_tmp])
+	{
+		free(data->cmd[l_tmp]);
+		data->cmd[l_tmp] = NULL;
+		data->cmd[l_tmp] = ft_strdup(tmp_cmd[z]);
+		l_tmp++;
+	}
+//	ft_putnbr_fd(i, 1);
+	return (0);
+}
+
+
+char	**ft_check_tab_spaces(t_data *data, char *cmd, int l)
+{
+	int i;
+	int count;
+	char **new_cmd;
+
+	i = 0;
+	count = 0;
+	new_cmd = NULL;
+	while (cmd[i])
+	{
+		if (cmd[i] == '-' && cmd[i + 1] != ' ')
+			count++;
+		i++;
+	}
+	if (count > 1)
+		new_cmd = ft_split(cmd, ' ');
+		int z = 0;
+	if (count > 1)
+	{
+	while (new_cmd[z])
+	{
+		ft_putstr_fd("XX", 1);
+		ft_putstr_fd(new_cmd[z], 1);
+		ft_putstr_fd("\n", 1);
+		z++;
+	}
+	}
+	if (count > 1)
+		ft_update_tab(data, new_cmd, l, count);
+	return (new_cmd);
+}
+
 
 int		ft_find_bin(t_data *data, int i, char **cmd, char **path_split)
 {
@@ -62,13 +146,19 @@ int		ft_get_absolute_path(t_data *data, char **cmd)
 //		ft_putstr_fd("*l*", 1);
 		while (data->cmd[l])
 		{
+			if (!(ft_strchr(data->cmd[l], '\"')))
+			{
+				cmd_tmp = ft_check_tab_spaces(data, data->cmd[l], l);
+//				ft_update_tab(data, cmd_tmp, l);
+			}
 			data->cmd[l] = ft_strtrim(data->cmd[l], " ");
 			data->cmd[l] = ft_strtrim(data->cmd[l], "\"");
+//			ft_check_tab_spaces(data->cmd[l]);
 //			if (ft_strchr(data->cmd[l], ' ') && ft_strchr(data->cmd[l], '-'))
 //				cmd_tmp = ft_split(cmd[l], ' ');
-			ft_putstr_fd("XX", 1);
-			ft_putstr_fd(cmd_tmp[l], 1);
-			ft_putstr_fd("\n", 1);
+//			ft_putstr_fd("XX", 1);
+//			ft_putstr_fd(cmd_tmp[l], 1);
+//			ft_putstr_fd("\n", 1);
 			l++;
 		}
 //		data->cmd[0] = ft_strtrim(cmd[0], " ");
